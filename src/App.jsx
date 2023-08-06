440598d45e893086b1f1419df542d47adbe043ab
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-08-05 22:19:16 -0300

    Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/header";
 
 export function App() {
 
   return (
     <>
-      
+      <Header/>
       <Post
         author="Allan"
         content="arglqkwrngqw´grpol qawr´goiqwrg´~qwplongk qw´rogjkwqg"
