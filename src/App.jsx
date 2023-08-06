79fe8004e8d9a293e306fe5c7266282eb24cb8d6
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-08-06 20:19:07 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { Post } from "./Post";
 import { Header } from "./components/header";
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
 
 export function App() {
 
   return (
-    <>
-      <Header/>
-      <Post
-        author="Allan"
-        content="arglqkwrngqw´grpol qawr´goiqwrg´~qwplongk qw´rogjkwqg"
-      />
-    </>
+    <div>
+    <Header />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post
+          author="Diego Fernandes"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+        />
+        <Post
+          author="Gabriel Buzzi"
+          content="Um novo post muito legal"
+        />
+      </main>
+    </div>
+  </div>
   )
 }
 
